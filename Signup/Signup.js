7f6c9aae4a59c8831565ab9a3b4d5a42856b77cb
Amusan Toluwanimi <emmanueltoluwanimi01@gmail.com>
2020-12-08 04:05:35 +0100

    trying to regex

--- Signup/Signup.js
+++ Signup/Signup.js
@@ -1,3 +1,7 @@
+//variables
+var regex;
+var fn;
+
 // function for tabs.
 document.getElementById("signup-tab").click();
 
@@ -18,4 +22,39 @@ function tabMenu(evt, tabName) {
 
     document.getElementById(tabName).style.display = 'block';
     evt.currentTarget.className += 'active';
+}
+
+$('.btn1').click(function (event) {
+    event.preventDefault();
+
+})
+$('#signup').on('keyup', function () {
+    validate($(this));
+})
+function validate(action) {
+    if (action.find('[placeholder="Fullname"]').focus()) {
+        regex = /[a-zA-Z]$/;
+        fn = action.find('[placeholder="Fullname"]').val();
+        console.log(fn);
+        
+        if (regex.test(fn)) {
+            $('#signup').find('[placeholder="Fullname"]').css('color', 'black');
+        }
+        else {
+            $('#signup').find('[placeholder="Fullname"]').css('color', 'red');
+        }
+    }
+    else if (action.find('[placeholder="Email"]').focus()) {
+        // regex = /^[a-zA-Z0-9]*[@]{1}[a-z]{2,6}[.]{1}[a-z]{2,5}$/;
+        eml = action.find('[placeholder="Email"]').val();
+        console.log(eml); 
+          
+        if (regex.test(eml)) {
+            $('#signup').find('[placeholder="Email"]').css('color', 'black');
+        }
+        else {
+            $('#signup').find('[placeholder="Email"]').css('color', 'red');
+        }
+    }
+
 }
