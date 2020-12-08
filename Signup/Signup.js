b7cd1842d98cacf34036c724713ed69c792f51c7
Amusan Toluwanimi <emmanueltoluwanimi01@gmail.com>
2020-12-08 01:22:56 +0100

    ui completed for signuppage

--- Signup/Signup.js
+++ Signup/Signup.js
@@ -1,19 +1,21 @@
 // function for tabs.
-function tabMenu(evt, cityName) {
+document.getElementById("signup-tab").click();
+
+function tabMenu(evt, tabName) {
     var tabcontent;
     var tablinks;
 
     tabcontent = document.getElementsByClassName('tabcontent');
-    for (items of tabcontent) {
-        items.style.display = 'none';
+    for (var i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = 'none';
     }
-    
+
 
     tablinks = document.getElementsByClassName('tablinks');
     for (items of tablinks) {
         items.className = items.className.replace('active', '');
     }
 
-    document.getElementById(cityName).style.display = 'block';
+    document.getElementById(tabName).style.display = 'block';
     evt.currentTarget.className += 'active';
 }
