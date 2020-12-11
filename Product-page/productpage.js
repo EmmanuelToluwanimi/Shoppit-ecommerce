66e4d2e0f0dc7583ad8840db0b79ab729a0b13ae
Amusan Toluwanimi <emmanueltoluwanimi01@gmail.com>
2020-12-11 15:46:16 +0100

    few changes and product page started

---
+++ Product-page/productpage.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('#navbtn').click(function () {
+        $('#nav-abr').toggle('.displa')
+        document.getElementById('navbtn').classList.toggle('change')
+    })
+})
