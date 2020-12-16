392638f34dfde61f8c5000e850364000f5ad9068
Amusan Toluwanimi <emmanueltoluwanimi01@gmail.com>
2020-12-16 09:54:38 +0100

    satrted with main js file and placed all products in an array of objects

---
+++ main.js
@@ -0,0 +1,60 @@
+// object of all products.
+var products = [
+    {
+        id: 1,
+        title: 'Runnable Shoes',
+        price: 13.99,
+        spec: 'Runnable Shoes Medium size Fashion and for Gym activites, very comfy for Runnuing.',
+        imageUrl: '../Images/pic15.png'
+    },
+    {
+        id: 2,
+        title: 'Male Sneakers',
+        price: 19.99,
+        spec: 'Male Sneakeres Large Size Fashion Casual Formal Shoes-BLUE.',
+        imageUrl: '../Images/5.jpg'
+    },
+    {
+        id: 3,
+        title: 'Student Backpack',
+        price: 10.99,
+        spec: 'Student Backpack Large Size Fashion Casual Formal Student bags, very comfy even with loads.',
+        imageUrl: '../Images/pic7.png'
+    },
+    {
+        id: 4,
+        title: 'Iphone11 4/64GB',
+        price: 750.99,
+        spec: 'Iphone11 4/64GB Green 64G_original-new-apple-i-phone-11-dual-12-mp-c_variants-3',
+        imageUrl: '../Images/Green 64G_original-new-apple-i-phone-11-dual-12-mp-c_variants-3.jpg'
+    },
+    {
+        id: 5,
+        title: 'Female Shoes',
+        price: 199.99,
+        spec: 'Female Shoes Medium Size Fashion Casual Formal Shoes-BLACK.',
+        imageUrl: '../Images/pic2.png'
+    },
+    {
+        id: 6,
+        title: 'Smart Wrist Watch',
+        price: 10.99,
+        spec: 'Smart Wrist Watch Bluetooth GSM Phone For Android Samsung IPhone (white).',
+        imageUrl: '../Images/1.jpg'
+    },
+    {
+        id: 7,
+        title: 'Sunlight Detergent',
+        price: 10.99,
+        spec: 'Sunlight 2in1 Spring Sensations Handwash Washing Powder 900g.',
+        imageUrl: '../Images/sunlight.jpg'
+    },
+    {
+        id: 8,
+        title: 'Apple MacBook Pro',
+        price: 2999.99,
+        spec: 'Apple MacBook Pro - 16" Touch Bar - Intel Core I9 - 16GB - AMD Radeon Pro 5500M - 1TB SSD - Space Gray.',
+        imageUrl: '../Images/sunlight.jpg'
+    },
+
+]
